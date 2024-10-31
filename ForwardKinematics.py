89647551a9c7cdee0b18ad27c102f691a90c6db1
Dennis Escobar del Rio <descobardelrio@gmail.com>
2024-10-31 09:55:50 +0100

    Thrustdirections as a function of nozzle angles. Taken from mav_avero, avero_ctrl, helper_functions_symb_new_sim.py

--- ForwardKinematics.py
+++ ForwardKinematics.py
@@ -1,16 +1,30 @@
 ### From mav_avero/nodes/avero_ctrl/src/allocation_python_new/helper_functions_symb_new_sim.py
 import numpy as np
 
-
-def thrustvector_nozzle1_bodyframe(phi1, phi2):
+# Normal vectorr of the thrustvector exiting out of the nozzle 1 depending on the swivel nozzle angles phi11, phi12
+def thrustdirection_nozzle1_bodyframe(phi1, phi2):
   thrust_vector = np.array([
-    [-0.5 * np.sin(phi1) * np.cos(phi2) - 0.5 * np.sin(phi1) - 0.7071 * np.sin(phi2) * np.cos(phi1)],
-    [0.24183 * np.sin(phi1) * np.sin(phi2) - 0.171 * np.cos(phi1) * np.cos(phi2) - 0.171 * np.cos(phi1) - 0.46985 * np.cos(phi2) + 0.46985],
-    [-0.66447 * np.sin(phi1) * np.sin(phi2) + 0.46985 * np.cos(phi1) * np.cos(phi2) + 0.46985 * np.cos(phi1) - 0.171 * np.cos(phi2) + 0.171]
+    -0.5 * np.sin(phi1) * np.cos(phi2) - 0.5 * np.sin(phi1) - 0.7071 * np.sin(phi2) * np.cos(phi1),
+    0.24183 * np.sin(phi1) * np.sin(phi2) - 0.171 * np.cos(phi1) * np.cos(phi2) - 0.171 * np.cos(phi1) - 0.46985 * np.cos(phi2) + 0.46985,
+    -0.66447 * np.sin(phi1) * np.sin(phi2) + 0.46985 * np.cos(phi1) * np.cos(phi2) + 0.46985 * np.cos(phi1) - 0.171 * np.cos(phi2) + 0.171
   ])
   return thrust_vector
 
+def thrustdirection_nozzle2_bodyframe(phi1, phi2):
+  thrust_vector = np.array([
+    -0.209445 * np.sin(phi1) * np.sin(phi2) + 0.25 * np.sin(phi1) * np.cos(phi2) + 0.25 * np.sin(phi1) + 0.35355 * np.sin(phi2) * np.cos(phi1) + 0.1481 * np.cos(phi1) * np.cos(phi2) + 0.1481 * np.cos(phi1) + 0.4069 * np.cos(phi2) - 0.4069,
+    -0.12092 * np.sin(phi1) * np.sin(phi2) - 0.433 * np.sin(phi1) * np.cos(phi2) -0.433 * np.sin(phi1) - 0.61235 * np.sin(phi2) * np.cos(phi1) + 0.0855 * np.cos(phi1) * np.cos(phi2) + 0.0855 * np.cos(phi1) + 0.2349 * np.cos(phi2) - 0.2349,
+    -0.66447 * np.sin(phi1) * np.sin(phi2) + 0.46985 * np.cos(phi1) * np.cos(phi2) + 0.46985 * np.cos(phi1) - 0.171 * np.cos(phi2) + 0.171
+  ])
+  return thrust_vector
 
+def thrustdirection_nozzle3_bodyframe(phi1, phi2):
+  thrust_vector = np.array([
+    0.20945 * np.sin(phi1) * np.sin(phi2) + 0.25 * np.sin(phi1) * np.cos(phi2) + 0.25 * np.sin(phi1) + 0.35355 * np.sin(phi2) * np.cos(phi1) - 0.1481 * np.cos(phi1) * np.cos(phi2) - 0.1481 * np.cos(phi1) - 0.4069 * np.cos(phi2) + 0.4069,
+    -0.1209 * np.sin(phi1) * np.sin(phi2) + 0.433 * np.sin(phi1) * np.cos(phi2) + 0.433 * np.sin(phi1) + 0.61235 * np.sin(phi2) * np.cos(phi1) + 0.0855 * np.cos(phi1) * np.cos(phi2) + 0.0855 * np.cos(phi1) + 0.2349 * np.cos(phi2) - 0.2349,
+    -0.66447 * np.sin(phi1) * np.sin(phi2) + 0.46985 * np.cos(phi1) * np.cos(phi2) + 0.46985 * np.cos(phi1) - 0.171 * np.cos(phi2) + 0.171
+  ])
+  return thrust_vector
 
 # Homogeneous Transformation matrices from MAV's body frame to base nozzle frame
 # Contains 3x3 rotation matrix R and 3x1 translation vector t
@@ -35,4 +49,6 @@ T_body_to_nozzle3_base = np.array([
   [ 0.0,     0.0,    0.0,     0.0]
 ])
 
-print(thrustvector_nozzle1_bodyframe(2*np.pi,0))
+print(thrustdirection_nozzle1_bodyframe(0, 0))
+print(thrustdirection_nozzle2_bodyframe(0, 0))
+print(thrustdirection_nozzle3_bodyframe(0, 0))
