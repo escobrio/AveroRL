1b664cf53a6e32b0a83044152e905dff2bf68c29
Dennis Escobar del Rio <descobardelrio@gmail.com>
2024-12-05 23:11:47 +0100

    GOOD COMMIT TO PLOT ACTUATOR RESPONSE. 1° actuator models now corrected. Log reward with BaseCallback for tensorboard. Nasty list.append(dict) happened again. Correctly clip and plot fanspeeds.

--- src/GymEnvironment.py
+++ src/GymEnvironment.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gymnasium as gym
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
 from scipy.spatial.transform import Rotation as R
 import matplotlib.pyplot as plt
 import time
@@ -8,12 +9,13 @@ from ForwardKinematics import thrustdirections, r_BE
 from Quaternion import quaternion_rotate_vector
 from Plots import plot_episode
 from torch.utils.tensorboard import SummaryWriter
+import copy
 
 # Gymnasium environment to train RL agent
 class MavEnv(gym.Env):
     def __init__(self):
         super().__init__()
-        self.writer = None
+        # self.writer = None
 
         # Observation space: [lin_vel, ang_vel, gravity vector in body frame, fan_speeds, nozzle_angles]
         self.observation_space = gym.spaces.Box(
@@ -40,8 +42,8 @@ class MavEnv(gym.Env):
                               0, 0, 0,           # [16:19] angular acc [rad/s²]
                               0, 0, 0,           # [19:22] fan speeds \in [0, 1]
                               0, 0, 0, 0, 0, 0,  # [22:28] nozzle angles [rad]
-                              0, 0, 0,           # [28: 31] fan speeds setpoints \in [0, 1]
-                              0, 0, 0, 0, 0, 0]) # [31: 37] nozzle angles setpoints 
+                              0, 0, 0,           # [28:31] fan speeds setpoints \in [0, 1]
+                              0, 0, 0, 0, 0, 0]) # [31:37] nozzle angles setpoints 
         
         # Physical and simulation parameters
         self.mass = 5.218  # [kg]
@@ -53,8 +55,8 @@ class MavEnv(gym.Env):
         self.step_counter = 0
         self.dt = 0.01  # [s]
         
-    def set_writer(self, writer):
-        self.writer = writer
+    # def set_writer(self, writer):
+    #     self.writer = writer
     
     def reset(self, seed=None):
         super().reset(seed=seed)
@@ -102,21 +104,24 @@ class MavEnv(gym.Env):
         return obs, info
     
     # First order actuator models, k[Hz] = 1 / tau [s]
-    # tau [s] is the empirical time constant of the actuator state following a setpoint [s]
-    # state_dot_k = (1/tau) * (setpoint_k - state)
+    # tau [s] is the empirical time constant of the actuator state following a setpoint
+    # state_dot_k = (1/tau) * (setpoint_k - state_k)
     # state_{k+1} = state_k + state_dot_k * dT
     # action is change in actuator setpoint
     def first_order_actuator_models(self, action):
-        # Update nozzle angle [rad] according to first order model of error = setpoint - state
         nozzles_state = self.state[22:28]
-        nozzles_setpoint = nozzles_state + action[3:] / self.k_phi          # TODO: nozzle_setpoint = nozzle_state + phi_dot / k_phi_nominal
-        nozzles_dot = self.k_phi * (nozzles_setpoint - nozzles_state)       # TODO: nozzle_dot = self.k_phi_randomized * (nozzles_setpoint - nozzle_state)
+        nozzles_setpoint = self.state[31:37]
+        # Update setpoints and nozzle_angles [rad] according to first order model
+        nozzles_setpoint += action[3:] / self.k_phi                             # Update setpoint with inverse 1° model
+        nozzles_dot = self.k_phi * (nozzles_setpoint - nozzles_state)           # TODO: nozzle_dot = self.k_phi_randomized * (nozzles_setpoint - nozzle_state)
         nozzles_state += nozzles_dot * self.dt
 
-        # Update fan speed [PWM] according to first order model of error = setpoint - state
         fanspeeds_state = self.state[19:22]
-        fanspeeds_setpoint = fanspeeds_state + action[:3] / self.k_omega        # TODO: Same as above
-        fanspeeds_dot = self.k_omega * (fanspeeds_setpoint - fanspeeds_state)   # TODO: Here fanspeeds is normalized [-1,1], but k_omega still has to be converted
+        fanspeeds_setpoint = self.state[28:31]
+        # Update setpoints and normalized fanspeeds according to first order model
+        fanspeeds_setpoint += action[:3] / self.k_omega                         # Same as above
+        fanspeeds_setpoint = fanspeeds_setpoint.clip(0, 1)
+        fanspeeds_dot = self.k_omega * (fanspeeds_setpoint - fanspeeds_state)   # TODO: Same as above
         fanspeeds_state += fanspeeds_dot * self.dt
         fanspeeds_state = fanspeeds_state.clip(0, 1)
 
@@ -125,21 +130,19 @@ class MavEnv(gym.Env):
     # Compute thrust vectors [N] of the 3 nozzles in body frame
     def compute_thrust_vectors(self, nozzles_angles, fanspeeds):
         # thrust = k_f * (PWM - 1050)² * normal_vector
-        # In this case, using the actual PWM signal, so -1050 is NOT already subtracted:
-        fanspeeds_pwm = 1050 + fanspeeds * 900
+        fanspeeds_pwm = 1050 + fanspeeds * 900                                  # Scale normalized fanspeeds \in [0, 1] to PWM signal \in [1050, 1950]
         fanspeeds_squared = np.square(fanspeeds_pwm - 1050)[:, np.newaxis]
         thrust_vectors = self.k_f * fanspeeds_squared * thrustdirections(nozzles_angles)
         return thrust_vectors
     
     def compute_forces_and_torques(self, thrust_vectors):
-        """Compute net force and torque from thrust vectors."""
         # Net force is sum of all thrust vectors
         force = np.sum(thrust_vectors, axis=0)
         
         # Compute torques from each thrust
+        nozzle_states = np.zeros(6) # TODO use actual phi commands
+        r_BE_1, r_BE_2, r_BE_3 = r_BE(nozzle_states)
         torque = np.zeros(3)
-        action_phi = np.zeros(6) # TODO use actual phi commands
-        r_BE_1, r_BE_2, r_BE_3 = r_BE(action_phi)
         torque += np.cross(r_BE_1, thrust_vectors[0])
         torque += np.cross(r_BE_2, thrust_vectors[1])
         torque += np.cross(r_BE_3, thrust_vectors[2])
@@ -194,31 +197,32 @@ class MavEnv(gym.Env):
             nozzle_setpoints
         ])
 
-        obs = np.concatenate([lin_vel, ang_vel, g_bodyframe, fanspeeds, nozzles_angles])
+        obs = np.concatenate([lin_vel, ang_vel, g_bodyframe, fanspeeds, nozzles_angles])    # TODO: add fanspeeds_setpoint and nozzles_setpoint
 
         # Reward Function
         lin_vel_penalty = np.linalg.norm(lin_vel)
         ang_vel_penalty = np.linalg.norm(ang_vel)
         action_penalty = np.linalg.norm(action)
+        reward_info = {"lin_vel_penalty": lin_vel_penalty, "ang_vel_penalty": ang_vel_penalty, "action_penalty": action_penalty}
         reward = 1 - 0.1 * lin_vel_penalty - 0.1 * ang_vel_penalty - 0.01 * action_penalty
 
         # Log the reward parts to TensorBoard
-        if self.writer:
-            self.writer.add_scalar("reward/lin_vel_penalty", - lin_vel_penalty, self.step_counter)
-            self.writer.add_scalar("reward/ang_vel_penalty", - ang_vel_penalty, self.step_counter)
-            self.writer.add_scalar("reward/action_penalty", - action_penalty, self.step_counter)
-            self.writer.add_scalar("reward/total", reward, self.step_counter)
+        # if self.writer:
+        #     self.writer.add_scalar("reward/lin_vel_penalty", - lin_vel_penalty, self.step_counter)
+        #     self.writer.add_scalar("reward/ang_vel_penalty", - ang_vel_penalty, self.step_counter)
+        #     self.writer.add_scalar("reward/action_penalty", - action_penalty, self.step_counter)
+        #     self.writer.add_scalar("reward/total", reward, self.step_counter)
 
         # Terminate if velocity is going crazy
         if (np.any(np.abs(lin_vel) > 5) or np.any(np.abs(ang_vel) > 5)):
             terminated = True        
         
-        # Check if truncated
+        # Truncate episode after 1000 timesteps
         self.step_counter += 1
         if (self.step_counter > 1_000):
             truncated = True
 
-        info = {"state": self.state}
+        info = {"state": self.state, "reward": reward_info}
         
         return obs, reward, terminated, truncated, info
     
@@ -232,14 +236,26 @@ def train_MAV():
     # model = PPO.load("data/ppo_mav_model", env=env)
     model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./logs/ppo_mav/")
 
-    writer = SummaryWriter(log_dir="./logs/ppo_mav/")
+    # writer = SummaryWriter(log_dir="./logs/ppo_mav/")
 
-    env.set_writer(writer)
+    # env.set_writer(writer)
 
-    model.learn(total_timesteps=1_000_000)
+    model.learn(total_timesteps=50_000, callback=TensorboardCallback())
 
     model.save("data/ppo_mav_model")
 
+class TensorboardCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super().__init__(verbose)
+
+    def _on_step(self) -> bool:
+        value = np.random.random()
+        # print(f"\n---\nself.locals: {self.locals}\n---\nself.globals:")
+        # print(f"\n---\nrewards: {self.locals['infos'][0]['reward']['lin_vel_penalty']}")
+        self.logger.record("reward/lin_vel_penalty", - self.locals['infos'][0]['reward']['lin_vel_penalty'])
+        self.logger.record("reward/ang_vel_penalty", - self.locals['infos'][0]['reward']['ang_vel_penalty'])
+        self.logger.record("reward/action_penalty", - self.locals['infos'][0]['reward']['action_penalty'])
+        return True    
 
 def evaluate_model():
     env = MavEnv()
@@ -258,24 +274,29 @@ def evaluate_model():
           nozzles_angles {info['state'][22:28]}""")
 
     # Record states and actions
-    observations = [obs]
-    infos = [info]
+    observations = [copy.deepcopy(obs)]
+    infos = [copy.deepcopy(info)]
     actions = []
     rewards = []
 
     terminated, truncated = False, False
-    
+    action_constant = np.random.uniform(low=-1, high=1, size=9)
+
     # Run one episode
     while not (terminated or truncated):
 
         action, _states = model.predict(obs, deterministic=True)
+        # TODO: Remove hardcoded action:
+        action = action_constant
 
         obs, reward, terminated, truncated, info = env.step(action)
 
-        observations.append(obs)
-        infos.append(info)
-        actions.append(action)
-        rewards.append(reward)
+        # TODO: Nastyyy bug where appending dictionairy info to infos messes up everything.
+        # Solved with copy.deepcopy()
+        observations.append(copy.deepcopy(obs))
+        infos.append(copy.deepcopy(info))
+        actions.append(copy.deepcopy(action))
+        rewards.append(copy.deepcopy(reward))
     
     plot_episode(observations, infos, actions, rewards)
     
